99ea426c1727a09cf8d0d5c98a8a908127808c34
TrentonFrey <91774460+TrentonFrey@users.noreply.github.com>
2022-10-13 02:10:35 -0400

    Add files via upload

---
+++ Database(OLD)/README.md
@@ -0,0 +1,18 @@
+# CS-340
+CS-340-T5626 Client/Server Development
+
+
+How do you write programs that are maintainable, readable, and adaptable? Especially consider your work on the CRUD Python module from Project One, which you used to connect the dashboard widgets to the database in Project Two. What were the advantages of working in this way? How else could you use this CRUD Python module in the future?
+
+When you are creating code it is important to have scaleability in mind. I could use this module to save time when creating similar databaes. This will save alot of dev time. 
+
+
+
+
+How do you approach a problem as a computer scientist? Consider how you approached the database or dashboard requirements that Grazioso Salvare requested. How did your approach to this project differ from previous assignments in other courses? What techniques or strategies would you use in the future to create databases to meet other client requests?
+
+The big thing about solving this problems was having to keep end user experince in mind thoughout the whole project. Trying to keep the databse fast and the user dashbord easy to uderstand and use was key. 
+
+What do computer scientists do, and why does it matter? How would your work on this type of project help a company, like Grazioso Salvare, to do their work better?
+
+Computer scientiests are the back bone of most comanies today. This is a bid deal for infustructor and creating new products. Alot of companyios only offer digital prodcuts and this is where computer scientesits come in. 
---
+++ Database(OLD)/crud.py
@@ -0,0 +1,88 @@
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+
+class AnimalShelter(object):
+    """ CRUD operations for Animal collection in MongoDB """
+
+    def __init__(self, username=None, password=None):
+        # Initializing the MongoClient. This helps to 
+        # access the MongoDB databases and collections. 
+        if username and password:
+            self.client = MongoClient('mongodb://%s:%s@localhost:52320' % (aacuser, 12345))
+        else:
+            self.client = MongoClient('mongodb://localhost:52320')
+        self.database = self.client['project']
+
+# Complete this create method to implement the C in CRUD.
+    def create(self, data):
+        if data is not None:
+            insert = self.database.animals.insert(data)  # data should be dictionary 
+            if insert!=0:
+                return True
+            else:
+                return False           
+        else:
+            raise Exception("Nothing to save, because data parameter is empty")
+
+
+    # Create method to implement the R in CRUD.
+    def read(self,criteria=None):
+
+      
+        if criteria:
+               
+            
+            data = self.database.animals.find(criteria,{"_id":False})
+        else:
+         
+            data = self.database.animals.find( {} , {"_id":False})
+
+        return data
+        
+        
+        
+        
+        
+       #U in crud 
+   def update(self, fromTarget, toTarget, count):
+        if fromTarget is not None:
+            if count == 1:
+                update_result = self.database.animals.update_one(fromTarget, toTarget)
+                pprint("Count- " + str(update_result.matched_count) + ", Count- " + str(update_result.modified_count))
+                if update_result.modified_count == 1:
+                    
+                    print(update_result)
+                    return True
+                    
+                else:
+                   
+                    return False
+                    
+           
+
+    #d in crud
+    def obtainDeleteData(self):
+        #key
+        for i in range(1):
+            key = input("Enter delete key: ")
+            value = input("Enter delete value: ")
+            userDeleteTarget.update({key: value})
+    #delete 
+    def deleteData(self, target, count):
+        if target is not None:
+            if count == 1:
+                try:
+                    delete_result = self.database.animals.delete_one(target)
+                    pprint("Count- " + str(delete_result.deleted_count))
+                    if delete_result.deleted_count == 0:
+                        
+                        print(delete_result)
+                        return True
+                    else:
+                        
+                        print(delete_result)
+                        return True
+                except Exception as e:
+                    
+            
+            return False
